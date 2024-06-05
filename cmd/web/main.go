9c6e37db4b9657ccdd7279ae72180da857d46412
Christian Wallace <christian.wallace63@gmail.com>
2024-06-05 09:14:03 -0500

    Restructuring Logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", getHome)
 	mux.HandleFunc("GET /snippet/view/{id}", getSnippetView)
 	mux.HandleFunc("GET /snippet/create", getSnippetCreate)
-
 	mux.HandleFunc("POST /snippet/create", postSnippetCreate)
 
-	log.Print("starting server on :4000")
+	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+
+	logger.Error(err.Error())
+	os.Exit(1)
 }
