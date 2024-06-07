f7d2a1007d57c0aa68b412a368da39d3921463f2
Christian Wallace <christian.wallace63@gmail.com>
2024-06-07 09:01:13 -0500

    Cleaning up route handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,20 +21,9 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.getHome)
-	mux.HandleFunc("GET /snippet/view/{id}", app.getSnippetView)
-	mux.HandleFunc("GET /snippet/create", app.getSnippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.postSnippetCreate)
-
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
-
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
