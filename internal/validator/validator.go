568b1851482057f8932b262933477b7d0fa6a767
Christian Wallace <christian.wallace63@gmail.com>
2024-07-07 08:58:03 -0500

    U: 10.4

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -11,11 +11,16 @@ var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](
 
 // Define a custom Validator type which holds a map of validation errors.
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 func (v *Validator) AddFieldError(key, message string) {
