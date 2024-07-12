fd67e567cff79ecbd261c313a54d97ee9d827b79
Christian Wallace <christian.wallace63@gmail.com>
2024-07-12 07:20:29 -0500

    A: 11.2

--- internal/models/users.go
+++ internal/models/users.go
@@ -48,9 +48,7 @@ func (m *UserModel) Insert(name, email, password string) error {
 func (m *UserModel) Authenticate(email, password string) (int, error) {
     var id int
     var hashedPassword []byte
-
     stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
-
     err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
     if err != nil {
         if errors.Is(err, sql.ErrNoRows) {
@@ -59,7 +57,6 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
             return 0, err
         }
     }
-
     err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
     if err != nil {
         if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
@@ -68,10 +65,12 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
             return 0, err
         }
     }
-
     return id, nil
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
-	return false, nil
+	var exists bool
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
